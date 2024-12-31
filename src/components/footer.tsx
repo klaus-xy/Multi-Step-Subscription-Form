39d61d50ad98486291827bfb0710312ad4a5c083
Klaus117 <oyesikupeace@gmail.com>
2024-12-31 20:15:16 +0100

    Layout and setup

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="">
+      <p>coded by klaus117</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/forms/multi-step-form.tsx
+++ src/components/forms/multi-step-form.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const MultiStepForm = () => {
   return (
-    <div>
-      <span>Form</span>
+    <div className="min-h-screen w-full max-w-5xl flex flex-col p-4 bg-slate-400">
+      {/* BLOCK 1 */}
+      <div className=" bg-emerald-400">Steps</div>
+
+      {/* BLOCK 2 */}
+      <div className="flex flex-col flex-1 bg-pink-200">
+        <div className="flex-1">Form Contents</div>
+        <div>Nav Bar</div>
+      </div>
     </div>
   );
 };
