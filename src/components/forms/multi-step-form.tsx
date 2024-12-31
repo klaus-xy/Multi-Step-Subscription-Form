9d6989f40969517bb5a0939d25cbe812ef800341
Klaus117 <oyesikupeace@gmail.com>
2024-12-31 13:47:41 +0100

    Init

---
+++ src/components/forms/multi-step-form.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MultiStepForm = () => {
+  return (
+    <div>
+      <span>Form</span>
+    </div>
+  );
+};
+
+export default MultiStepForm;
---
+++ src/components/plan-selection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlanSelection = () => {
+  return <div>PlanSelection</div>;
+};
+
+export default PlanSelection;
