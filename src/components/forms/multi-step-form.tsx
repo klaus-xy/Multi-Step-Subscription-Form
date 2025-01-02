a7ac808148346f45e16146b4fbe90a6e6f965ebc
Klaus117 <oyesikupeace@gmail.com>
2025-01-02 19:20:13 +0100

    Layout Updates

--- src/components/forms/multi-step-form.tsx
+++ src/components/forms/multi-step-form.tsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { Button } from "../ui/button";
 
 const MultiStepForm = () => {
   return (
     <div className="min-h-screen w-full max-w-5xl flex flex-col p-4 bg-slate-400">
       {/* BLOCK 1 */}
-      <div className=" bg-emerald-400">Steps</div>
+      <div className="min-h-52 bg-violet-300 bg-[url(/images/bg-sidebar-mobile.svg)] bg-cover bg-center flex items-start justify-center">
+        <div className="mt-8">Steps go here...</div>
+      </div>
 
       {/* BLOCK 2 */}
-      <div className="flex flex-col flex-1 bg-pink-200">
-        <div className="flex-1">Form Contents</div>
-        <div>Nav Bar</div>
+      <div className="flex flex-col flex-1 bg-pink-300 relative">
+        <div className="flex-1">
+          <div className="min-h-[400px] w-[90%] mx-auto bg-emerald-400 relative -top-12 p-4">
+            Form Contents
+          </div>
+        </div>
+        <div className="flex justify-between items-center bg-red-300">
+          <Button>Prev</Button>
+          <Button>Next</Button>
+        </div>
       </div>
     </div>
   );
